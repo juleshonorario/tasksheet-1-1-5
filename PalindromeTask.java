630eabdcf9c9f95d396bf005cdf49692c39c9941
juleshonorario <153901269+juleshonorario@users.noreply.github.com>
2023-12-28 06:42:04 +0000

    tasksheet-1-1-5

--- PalindromeTask.java
+++ PalindromeTask.java
@@ -1,1 +1,22 @@
+import java.util.Scanner;
 
+public class PalindromeTask {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.println("Enter a word");
+        String string1 = input.nextLine();
+        StringBuilder sb = new StringBuilder(string1);
+        String reversed = sb.reverse().toString();
+
+        
+        System.out.println(sb.toString());
+        if (string1.equals(reversed)) {  
+        System.out.println("The input string is a palindrome.");
+        }
+        else {
+        System.out.println("The input string is not a palindrome.");
+        }
+        
+        input.close();
+    }
+}
